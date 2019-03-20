426fefdedd919474f3218613d4bf5b8b95ac326e
R. Stange <rsta2@o2online.de>
2019-03-20 13:00:42 +0100

    FIXED: Depth buffer was not cleared

--- sample/hello_raspi/objectviewer3d.cpp
+++ sample/hello_raspi/objectviewer3d.cpp
@@ -110,7 +110,7 @@ void CObjectViewer3D::UpdateView (void)
 	assert (m_bInitialized);
 
 	glClearColor (0.0f, 0.0f, 0.0f, 1.0f);
-	glClear (GL_COLOR_BUFFER_BIT);
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glMatrixMode (GL_MODELVIEW);
 
